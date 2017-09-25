b7edd8e41cdc595b0cffc9e8393e6ab460741120
Kate Shestakova <kate.shestakova@me.com>
2017-09-25 15:01:39 +0300

    delete unnecessary links

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,8 +8,6 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Link to="/search" style={{color: "black", marginRight: "20px"}}>ListResult</Link>
-        <Link to="/film/Attack on Titan" style={{color: "black"}}>FilmDescr</Link>
         {this.props.children}
       </div>
     )
