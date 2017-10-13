15631066a730d0a513aa59b41c6215d5656e2997
Katsiaryna_Shastakov <Katsiaryna_Shastakova@epam.com>
2017-10-13 17:23:40 +0300

    sorting

--- src/containers/ListResultContainer.js
+++ src/containers/ListResultContainer.js
@@ -1,9 +1,20 @@
 import { connect } from 'react-redux';
 import { ListResult } from '../components/ListResult';
+import { SortBy } from '../reducers/actions';
 
 const mapStateToProps = (state) => {
+  const sortedMovies = state.movies.slice();
+  sortedMovies.sort((a,b) => {
+    if(state.sortBy === SortBy.SORT_BY_RELEASE_DATE) {
+      return parseInt(a.release_year) - parseInt(b.release_year);
+    }
+    if(state.sortBy === SortBy.SORT_BY_RATING) {
+      return parseInt(b.rating) - parseInt(a.rating);
+    }
+  });
+  console.log(sortedMovies);
   return {
-    movies: state.movies
+    movies: sortedMovies
   }
 };
 
