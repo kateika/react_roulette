8a633e7a1d7e18654e9b6d449b8119be54e43719
Katsiaryna_Shastakov <Katsiaryna_Shastakova@epam.com>
2017-10-25 18:26:20 +0300

    test for list result page

--- tests/components/ListResult.test.js
+++ tests/components/ListResult.test.js
@@ -1,3 +1,28 @@
-/**
- * Created by Katsiaryna_Shastakov on 10/25/2017.
- */
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import { ListResult } from "../../src/components/ListResult";
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+configure({ adapter: new Adapter() });
+
+describe("single and plural version of text", () => {
+  describe("1 result", () => {
+    it("render single version", () => {
+      const wrapper = shallow(<ListResult movies={["1"]} />);
+      expect(wrapper.contains([
+        <span>1 result was found</span>
+      ]));
+    });
+
+  });
+
+  describe("many results", () => {
+    it("render single version", () => {
+      const wrapper = shallow(<ListResult movies={["1", "2"]} />);
+      expect(wrapper.contains([
+        <span>2 results was found</span>
+      ]));
+    });
+  });
+});
