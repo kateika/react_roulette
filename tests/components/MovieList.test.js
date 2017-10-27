8c6e52f174e580b7ea71bc05b9fa9f31436b62e8
Katsiaryna_Shastakov <Katsiaryna_Shastakova@epam.com>
2017-10-27 10:42:49 +0300

    unit test for passed to movie card properties

--- tests/components/MovieList.test.js
+++ tests/components/MovieList.test.js
@@ -28,3 +28,29 @@ describe("Filled search results", () => {
   });
 });
 
+describe("Movie list props for movie card", () => {
+  const movies = [{
+    title: "test title",
+    release_date: "1976-06-17",
+    poster: "/wajNlrVDkryVGTzDz9vZE6XKZnC.jpg",
+    id: 111,
+    type: "movie"
+  }];
+
+  const wrapper = shallow(<MovieList movies={movies} />);
+  const movieCardProps = wrapper.find("MovieCard").first().props();
+
+  it("passes modified year", () => {
+    expect(movieCardProps["release_date"]).toBe("1976");
+  });
+
+  it("passes modified url of the poster", () => {
+    expect(movieCardProps["poster"]).toBe("https://image.tmdb.org/t/p/w185_and_h278_bestv2/wajNlrVDkryVGTzDz9vZE6XKZnC.jpg");
+  });
+
+  it("passes not modified props", () => {
+    expect(movieCardProps["title"]).toBe("test title");
+    expect(movieCardProps["id"]).toBe(111);
+    expect(movieCardProps["type"]).toBe("movie");
+  });
+});
